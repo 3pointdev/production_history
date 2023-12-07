6ce22b13605975118f1a79ceee14b8a744ae9fd7
3pointdev <motors3996@gmail.com>
2023-12-07 16:16:40 +0900

    env clear

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -3,9 +3,6 @@
  */
 export const ServerUrlType = {
   BARO: process.env.NEXT_PUBLIC_BARO_URL,
-  APIS: process.env.NEXT_PUBLIC_APIS_URL,
-  EDGE: process.env.NEXT_PUBLIC_EDGE_API_URL,
-  WEBSOCKET: process.env.NEXT_PUBLIC_WEBSOCKET_URL,
 } as const;
 export type ServerUrlType = (typeof ServerUrlType)[keyof typeof ServerUrlType];
 
